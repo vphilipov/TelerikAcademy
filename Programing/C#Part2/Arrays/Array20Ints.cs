99ed51456359bc9f3072d450529ee705e42ad45f
Vesselin Philipov <vess.ph@gmail.com>
2013-02-16 03:45:45 +0200

    Arrays h/w

---
+++ Programing/C#Part2/Arrays/Array20Ints.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Write a program that allocates array of 20 integers and initializes each element 
+ * by its index multiplied by 5. Print the obtained array on the console.
+ */
+
+namespace Array20Ints
+{
+    class Array20Ints
+    {
+        static void Main()
+        {
+            int[] numbers = new int[20];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i * 5;
+                Console.WriteLine("Element {0} = {1}", i, numbers[i]);
+            }
+        }
+    }
+}
